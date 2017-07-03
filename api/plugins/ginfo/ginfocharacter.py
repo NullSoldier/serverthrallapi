23e86b10f7ff59167dbed17c4b8b02fb04ef9066
Markus Chmelar <mexx@innovaptor.com>
2017-07-03 12:27:22 +0200

    Remove wrong on_delete rule for GinfoCharacter

--- api/plugins/ginfo/ginfocharacter.py
+++ api/plugins/ginfo/ginfocharacter.py
@@ -5,7 +5,6 @@ from api.models import Character
 class GInfoCharacter(models.Model):
     character = models.ForeignKey(
         Character,
-        on_delete=models.CASCADE,
         related_name="ginfo_character",
         related_query_name="ginfo_character",
     )
