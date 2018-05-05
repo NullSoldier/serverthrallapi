f9d0f28a48cd3553229b37fd62e6e68df0f0eda1
Jason Spafford <nullprogrammer@gmail.com>
2018-05-05 02:43:46 -0700

    Fix crash with missing field

--- api/views/servers.py
+++ api/views/servers.py
@@ -10,15 +10,16 @@ from .base import BaseView
 
 class ServersView(BaseView):
 
-    def post(self, request):
-        server = Server()
-        server.private_secret = uuid1()
-        server.save()
-
-        serialized = ServerAdminSerializer(server).data
-        return JsonResponse(serialized, status=200)
-
     def get(self, request):
         servers = Server.objects.only_active().with_character_count()
         serialized = ServerSerializer(servers, many=True).data
         return JsonResponse({'items': serialized})
+
+    def post(self, request):
+        server = Server()
+        server.private_secret = uuid1()
+        server.character_count = 0
+        server.save()
+
+        serialized = ServerAdminSerializer(server).data
+        return JsonResponse(serialized, status=200)
