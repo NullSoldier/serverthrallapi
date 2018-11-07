af4ac3af94f93f48603406c582deef4eb65097fd
Jason Spafford <nullprogrammer@gmail.com>
2018-11-07 11:52:54 -0800

    Reduce celery communication noise

--- startworker.sh
+++ startworker.sh
@@ -4,6 +4,9 @@ export NEW_RELIC_APP_NAME=serverthrallapi.celery
 
 newrelic-admin run-program \
     celery worker \
+    --without-gossip \
+    --without-mingle \
+    --without-heartbeat \
     --app=serverthrallapi.celery_app \
     --beat \
     --scheduler django
